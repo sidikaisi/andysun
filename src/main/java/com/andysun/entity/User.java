8d448f6ef64d9059ecd34714d381510878d7eed9
zhangxiaojun <zhangxiaojun@tezign.com>
2019-07-26 17:08:13 +0800

    自动生成的代码

---
+++ src/main/java/com/andysun/entity/User.java
@@ -0,0 +1,35 @@
+package com.andysun.entity;
+
+import java.time.LocalDate;
+import com.andysun.entity.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author zhangxiaojun
+ * @since 2019-07-26
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+public class User extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 生日
+     */
+    private LocalDate birthday;
+
+
+}
---
+++ src/main/java/com/andysun/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.andysun.mapper;
+
+import com.andysun.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zhangxiaojun
+ * @since 2019-07-26
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
